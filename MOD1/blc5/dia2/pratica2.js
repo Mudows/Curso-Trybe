be40a9ff6f61f9408ad130df93826521ed4a6e63
Diego C. Cezar <dmcezar@gmail.com>
2022-03-20 18:04:13 -0300

    Pratica 2 do bloco 5, dia 2 - Concluídos.

---
+++ MOD1/blc5/dia2/pratica2.js
@@ -0,0 +1,12 @@
+// Crie um irmão para elementoOndeVoceEsta.
+document.getElementById('pai').appendChild(document.createElement('section'));
+
+// Crie um filho para elementoOndeVoceEsta.
+document.getElementById('elementoOndeVoceEsta').appendChild(document.createElement('section'));
+
+// Crie um filho para primeiroFilhoDoFilho.
+document.getElementById('primeiroFilhoDoFilho').appendChild(document.createElement('section'));
+
+// A partir desse filho criado, acesse terceiroFilho.
+//document.getElementById('primeiroFilhoDoFilho').lastElementChild
+
