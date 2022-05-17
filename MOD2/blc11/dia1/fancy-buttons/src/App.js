cfc6b780a2f86c52928e945d79600ee02c4ca7eb
Diego Moraes Cezar <dmcezar@gmail.com>
2022-05-17 16:06:07 -0300

    Prática 3: FancyButtons - Estados e Eventos.

--- MOD2/blc11/dia1/fancy-buttons/src/App.js
+++ MOD2/blc11/dia1/fancy-buttons/src/App.js
@@ -2,22 +2,34 @@ import React from "react";
 import "./App.css";
 
 export default class App extends React.Component {
-
   constructor() {
-    super()
-    this.escreve = this.escreve.bind(this)
+    super();
+    this.escreve = this.escreve.bind(this);
+    this.state = {
+      pBotao: 0,
+      sBotao: 0,
+      tBotao: 0,
+    };
   }
 
   escreve(event) {
-    return console.log(event.target.innerText);
+    if (event.target.id === 'bt1') this.setState((anterior, _props) => ({ pBotao: anterior.pBotao + 1 }));
+    if (event.target.id === 'bt2') this.setState((anterior, _props) => ({ sBotao: anterior.sBotao + 1 }));
+    if (event.target.id === 'bt3') this.setState((anterior, _props) => ({ tBotao: anterior.tBotao + 1 }));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.escreve}>Clica nessa Pleura!</button>
-        <button onClick={this.escreve}>E nessa aqui</button>
-        <button onClick={this.escreve}>Nessa pleura também!</button>
+        <button onClick={this.escreve} id="bt1">
+          {this.state.pBotao}
+        </button>
+        <button onClick={this.escreve} id="bt2">
+          {this.state.sBotao}
+        </button>
+        <button onClick={this.escreve} id="bt3">
+          {this.state.tBotao}
+        </button>
       </div>
     );
   }
